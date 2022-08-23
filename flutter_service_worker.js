4a4bfc2626d3c02c65b07de41608220a173aa447
HieuTranTrung <hieu.trantrung1204@gmail.com>
2022-08-23 22:19:12 +0700

    Deploy image list

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "27a12a4b75cd15d21978541576a51ffa",
-"/": "27a12a4b75cd15d21978541576a51ffa",
-"main.dart.js": "229656091b537da65dba2cbd0bacf991",
-"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
+"index.html": "b12530699ee359775d2fecaaa0b9244a",
+"/": "b12530699ee359775d2fecaaa0b9244a",
+"main.dart.js": "f147bbea9b62c6ebd8caf48df341d751",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -18,11 +17,7 @@ const RESOURCES = {
 "assets/NOTICES": "326b650fff915b46467fb44fdf37617b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b"
 };
 
 // The application shell files that are downloaded before a service worker can
