bbd2b49b6d211a4b793603af1e5933aa8f6ff150
Hiếu <hieu.trantrung1204@gmail.com>
2023-03-05 12:48:21 +0700

    Deploy 1.0.5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:image_widget/image_widget.dart';
+import 'package:imagewidget/image_widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
