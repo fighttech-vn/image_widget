5c9e461b5fca541d39d7762742e4df922a2360d5
Bảo Huy <>
2023-04-29 14:42:39 +0700

    update(Export): update export package dependencies

--- lib/imagewidget.dart
+++ lib/imagewidget.dart
@@ -1,5 +1,11 @@
 library imagewidget;
 
+// dependencies
+export 'package:extended_image/extended_image.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:lottie/lottie.dart';
+
+// source
 export 'src/entities/image_info.dart';
 export 'src/image_list_widget.dart';
 export 'src/image_widget.dart';
